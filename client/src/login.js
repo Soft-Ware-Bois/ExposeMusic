49339a3fe12e6d2e763a5ba681eb40728c1f4f40
fc410 <fc410@humboldt.edu>
2021-10-20 22:28:39 -0700

    downloaded required libraries
    bootstrap
    react-bootstrap

--- client/src/login.js
+++ client/src/login.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
 
 const AUTH_URL = `https://accounts.spotify.com/authorize?
-client_id={Your Client ID}&
+client_id=dd37f5158f4d4f5eb0c40f0d05c95121&
 response_type=code&redirect_uri=http://localhost:3000
 &scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20
 user-read-playback-state%20user-modify-playback-state`
